7e8201153faa40a31ece475d002195f8886c3d7e
Wojciech <wojciech.mista@hotmail.com>
2022-02-23 09:37:44 +0100

    Make onBeforeUnload disable if env is development

--- src/misc.ts
+++ src/misc.ts
@@ -503,3 +503,6 @@ export const combinedMultiAutocompleteChoices = (
   selected: MultiAutocompleteChoiceType[],
   choices: MultiAutocompleteChoiceType[]
 ) => uniqBy([...selected, ...choices], "value");
+
+export const isInDevelopment =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
